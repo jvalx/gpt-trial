855dc6faa2b77a388a246d9eb3e7a1c3af743f74
Juan Valderrama <jvalderr@usc.edu>
2025-07-31 21:46:43 -0400

    Update fetch_hpd_vacate.py

--- etl/fetch_hpd_vacate.py
+++ etl/fetch_hpd_vacate.py
@@ -1,13 +1,15 @@
 import os, requests, pandas as pd
 
 def run():
-    url = (
-        "https://data.cityofnewyork.us/resource/tb8q-a3ar.json"
-        "?$where=actual_rescind_date IS NULL"
-        "&$select=bbl"
-        "&$limit=50000"
-    )
-    r = requests.get(url, headers={"X-App-Token": os.getenv("NYC_APP_TOKEN")})
+    API = "https://data.cityofnewyork.us/resource/tb8q-a3ar.json"
+    token = os.getenv("NYC_APP_TOKEN")
+    params = {
+        "$where":      "actual_rescind_date IS NULL",
+        "$select":     "bbl",
+        "$limit":      50000,
+        "$$app_token": token,
+    }
+    r = requests.get(API, params=params, timeout=30)
     r.raise_for_status()
     df = pd.DataFrame(r.json())
     df["vacate_flag"] = 1
